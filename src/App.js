d67a77c010e3ed937116e6911bfc29f2952eabad
annaxor <roxannaortega@gmail.com>
2022-03-19 10:47:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://roxannaortega.com/" target="_blank" rel="noreferrer">
+          Roxanna Ortega Valdivieso
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/annaxor/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
