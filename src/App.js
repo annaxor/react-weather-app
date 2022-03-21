a066c121e389a41bd3d811e719b90ea1e8dbc3e8
annaxor <roxannaortega@gmail.com>
2022-03-21 17:20:18 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://roxannaortega.com/" target="_blank" rel="noreferrer">
-          Roxanna Ortega Valdivieso
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/annaxor/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather app</h1>
+        <button className="btn btn-primary">Hello</button>
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://roxannaortega.com/" target="_blank" rel="noreferrer">
+            Roxanna Ortega Valdivieso
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/annaxor/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
