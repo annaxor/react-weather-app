8d785980b6ac32766e558ec284e9f229aefba431
annaxor <roxannaortega@gmail.com>
2022-03-24 16:24:40 +0000

    api integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div class="container">
-        <Weather />
+        <Weather defaultCity="New York" />
 
         <footer>
           This project was coded by{" "}
