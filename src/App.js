348841ba19137f0ea8cea053bf91bf842e053fcc
annaxor <roxannaortega@gmail.com>
2022-03-26 11:49:38 +0000

    updated the search engine

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div class="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Caracas" />
 
         <footer>
           This project was coded by{" "}
